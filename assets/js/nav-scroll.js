ada31a6df8df628eb542cc30a2a595f4a3cec18a
cucurbiturilum <cucurbiturilum@gmail.com>
2020-07-28 16:06:01 +0300

    "What we do" page: added second section

--- assets/js/nav-scroll.js
+++ assets/js/nav-scroll.js
@@ -1,3 +1,10 @@
 $(window).scroll(function(){
     $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+$( window ).resize(function() {
+    if( $( window ).width() >= 768){
+        $('.navbar-collapse').removeClass('show');
+        $('.navbar-toggler').attr('aria-expanded', false);
+    }
+});

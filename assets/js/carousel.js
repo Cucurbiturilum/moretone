a3f9ae641cebc3dbce20feed1b8e8522704476d2
cucurbiturilum <cucurbiturilum@gmail.com>
2020-08-09 19:44:22 +0300

    Added "Approach" page

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-
+    $(".card-we-do").owlCarousel({
         responsive: {
             0: {
                 items: 1,
@@ -12,8 +11,8 @@ $(document).ready(function () {
                 loop: true,
                 stagePadding: 50,
             },
-            680:{
-               items: 3,
+            680: {
+                items: 3,
                 loop: true,
                 stagePadding: 60,
             },
@@ -25,4 +24,45 @@ $(document).ready(function () {
             }
         }
     });
+    $('.giving-back-carousel').owlCarousel({
+        stagePadding: 100,
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 1,
+    });
+    $('.testimonials-carousel').owlCarousel({
+        navContainer: '#owlCarouselNav',
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false,
+            },
+            768:{
+                items: 1,
+                loop: true,
+                stagePadding: 50,
+                margin: 20,
+                dots: false,
+                nav: true,
+            },
+            992:{
+                items: 1,
+                loop: true,
+                stagePadding: 70,
+                margin: 20,
+                dots: false,
+                nav: true,
+            },
+            1200: {
+                items: 1,
+                stagePadding: 120,
+                margin: 100,
+                loop: true,
+                dots: false,
+                nav: true,
+            }
+        }
+    })
 });
